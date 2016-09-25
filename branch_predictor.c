0011427aeff042714384d8d037e327aeb2792b0a
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2016-09-25 00:00:08 -0300

    Write two_bit_predictor and two_level_predictor functions
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

--- branch_predictor.c
+++ branch_predictor.c
@@ -10,72 +10,108 @@
 #define BTB_MISS_PREDICTED   4
 #define HIST_SIZE            4
 
-#define BRANCH_PREDICTOR     two_level_predictor
+#define BRANCH_PREDICTOR     two_bit_predictor
 
 struct branch_table {
   unsigned long int address;
   unsigned long int target;
-  unsigned char history;
+  unsigned char history; /* Two-level predictor */
+  unsigned char counter; /* Two-bit predictor */
   int valid;
 };
 
 static struct branch_table btb[BTB_SIZE];
 
-int get_opcode(const char *filename, char *assembly, char *opcode, unsigned long *address, unsigned long *size, unsigned *is_cond){
-    char buf[CHUNK];
-    char *sub_string = NULL;
-    char *tmp_ptr = NULL;
+int get_opcode(const char *filename, char *assembly, char *opcode, unsigned long *address, unsigned long *size, unsigned *is_cond) {
+  char buf[CHUNK];
+  char *sub_string = NULL;
+  char *tmp_ptr = NULL;
 
-    static FILE *file = NULL;
+  static FILE *file = NULL;
 
-    if (file == NULL) {
-        file = fopen(filename, "r");
-        if (file == NULL){
-            fprintf(stderr, "Could not open file.\n");
-            exit(1);
-        }
+  if (file == NULL) {
+    file = fopen(filename, "r");
+    if (file == NULL){
+      fprintf(stderr, "Could not open file.\n");
+      exit(1);
+    }
+  }
+
+  if(!fgets(buf, sizeof buf, file))
+    return (0);
+
+  // Check trace file
+  int i=0, count=0;
+
+  while (buf[i] != '\0') {
+    count += (buf[i] == ';');
+    i++;
+  }
+
+  if (count != 4) {
+    fprintf(stderr, "Error reading trace (Wrong  number of fields)\n");
+    fprintf(stderr, "%s", buf);
+    exit(2);
+  }
+
+  // ASM
+  sub_string = strtok_r(buf, ";", &tmp_ptr);
+  strcpy(assembly, sub_string);
+
+  // Operation
+  sub_string = strtok_r(NULL, ";", &tmp_ptr);
+  strcpy(opcode, sub_string);
+
+  // Address
+  sub_string = strtok_r(NULL, ";", &tmp_ptr);
+  *address = strtoul(sub_string,NULL,10);
+
+  // Size
+  sub_string = strtok_r(NULL, ";", &tmp_ptr);
+  *size = strtoul(sub_string,NULL,10);
+
+  // Conditional
+  sub_string = strtok_r(NULL, ";", &tmp_ptr);
+  *is_cond = (sub_string[0] == 'C') ? 1: 0;
+
+  return 1;
+}
+
+
+void not_taken_predictor(unsigned int index, unsigned long address, unsigned long size, unsigned long next_address, unsigned int added_recently, unsigned char *hit) {
+  if(address + size == next_address) {
+    *hit = 1;
+  } else {
+    *hit = 0;
+  }
+}
+
+void two_bit_predictor(unsigned int index, unsigned long address, unsigned long size, unsigned long next_address, unsigned int added_recently, unsigned char *hit) {
+  if(address + size == next_address && added_recently == 0) {
+    if(btb[index].counter < 2) {
+      *hit = 1;
+    } else {
+      *hit = 0;
     }
 
-    if(!fgets(buf, sizeof buf, file))
-        return (0);
-
-    // Check trace file
-    int i=0, count=0;
-    while (buf[i] != '\0')
-    {
-        count += (buf[i] == ';');
-        i++;
+    if(btb[index].counter > 0) {
+      --btb[index].counter;
     }
-    if (count != 4)
-    {
-        fprintf(stderr, "Error reading trace (Wrong  number of fields)\n");
-        fprintf(stderr, "%s", buf);
-        exit(2);
+  } else {
+    if(added_recently == 0) {
+      if(btb[index].counter < 2 || btb[index].target != next_address) {
+        *hit = 0;
+      } else {
+        *hit = 1;
+      }
+
+      if(btb[index].counter < 3) {
+        ++btb[index].counter;
+      }
     }
+  }
 
-    //printf("%s\n", buf);
-
-    // ASM
-    sub_string = strtok_r(buf, ";", &tmp_ptr);
-    strcpy(assembly, sub_string);
-
-    // Operation
-    sub_string = strtok_r(NULL, ";", &tmp_ptr);
-    strcpy(opcode, sub_string);
-
-    // Address
-    sub_string = strtok_r(NULL, ";", &tmp_ptr);
-    *address = strtoul(sub_string,NULL,10);
-
-    // Size
-    sub_string = strtok_r(NULL, ";", &tmp_ptr);
-    *size = strtoul(sub_string,NULL,10);
-
-    // Conditional
-    sub_string = strtok_r(NULL, ";", &tmp_ptr);
-    *is_cond = (sub_string[0] == 'C') ? 1: 0;
-
-    return 1;
+  btb[index].target = next_address;
 }
 
 void two_level_predictor(unsigned int index, unsigned long address, unsigned long size, unsigned long next_address, unsigned int added_recently, unsigned char *hit) {
@@ -124,16 +160,16 @@ int main(int argc, const char *argv[]) {
   unsigned long cycles = 0;
   unsigned long acum_hit = 0, acum_miss = 0, acum_miss_pred = 0;
   unsigned int is_cond, next_is_cond; 
-  unsigned int i, added_recently;
+  unsigned int index, added_recently;
 
   if(argc < 2) {
     fprintf(stdout, "Uso: %s <trace file>\n", argv[0]);
     exit(0);
   }
 
-  for(i = 0; i < BTB_SIZE; ++i) {
-    btb[i].address = 0;
-    btb[i].valid = 0;
+  for(index = 0; index < BTB_SIZE; ++index) {
+    btb[index].address = 0;
+    btb[index].valid = 0;
   }
 
   size = 0;
@@ -142,35 +178,36 @@ int main(int argc, const char *argv[]) {
     next_size = 0;
 
     if(strncmp(opcode, "OP_BRANCH", 9) == 0) {
-      i = address & 63;
+      index = address & 63;
       added_recently = 0;
 
-      if(btb[i].valid == 0 || btb[i].address != address) {
+      if(btb[index].valid == 0 || btb[index].address != address) {
+        btb[index].address = address;
+        btb[index].valid = 1;
+        btb[index].history = 0;
+        btb[index].counter = 0;
         cycles += BTB_MISS;
         acum_miss += BTB_MISS;
-        btb[i].address = address;
-        btb[i].valid = 1;
-        btb[i].history = 0;
         added_recently = 1;
       }
 
       if(get_opcode(argv[1], assembly, opcode, &next_address, &next_size, &next_is_cond)) {
         if(is_cond) {
-          BRANCH_PREDICTOR(i, address, size, next_address, added_recently, &hit);
+          BRANCH_PREDICTOR(index, address, size, next_address, added_recently, &hit);
 
-          if(hit == 1) {
-            cycles += BTB_HIT;
-            acum_hit += BTB_HIT;
-          } else {
-            cycles += BTB_MISS_PREDICTED;
-            acum_miss_pred += BTB_MISS_PREDICTED;
+          if(added_recently == 0) {
+            if(hit == 1) {
+              cycles += BTB_HIT;
+              acum_hit += BTB_HIT;
+            } else {
+              cycles += BTB_MISS_PREDICTED;
+              acum_miss_pred += BTB_MISS_PREDICTED;
+            }
           }
-        } else {
+        } else if(added_recently == 0) {
           cycles += BTB_HIT;
           acum_hit += BTB_HIT;
         }
-      } else {
-        btb[i].target = 0;
       }
     } else {
       ++cycles;
